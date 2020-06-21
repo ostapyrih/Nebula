ac70352c45db2df98ebd0640b49143d820586906
Ostap Pyrih <ostapyrih@gmail.com>
2020-06-21 16:49:05 +0200

    Added vector4d

--- src/math/vec4.cpp
+++ src/math/vec4.cpp
@@ -3,3 +3,92 @@
 //
 
 #include "vec4.h"
+
+namespace nebula::math {
+
+    vec4::vec4() : x(0.0f), y(0.0f), z(0.0f), w(0.0f) {}
+
+    vec4::vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
+
+    vec4& vec4::Add(const vec4& other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        w += other.w;
+
+        return *this;
+    }
+
+    vec4& vec4::Subtract(const vec4& other) {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        w -= other.w;
+
+        return *this;
+    }
+
+    vec4& vec4::Multiply(const vec4& other) {
+        x *= other.x;
+        y *= other.y;
+        z *= other.z;
+        w *= other.w;
+
+        return *this;
+    }
+
+    vec4& vec4::Divide(const vec4& other) {
+        x /= other.x;
+        y /= other.y;
+        z /= other.z;
+        w /= other.w;
+
+        return *this;
+    }
+
+    vec4 operator+(vec4 left, const vec4& right) {
+        return left.Add(right);
+    }
+
+    vec4 operator-(vec4 left, const vec4& right) {
+        return left.Subtract(right);
+    }
+
+    vec4 operator*(vec4 left, const vec4& right) {
+        return left.Multiply(right);
+    }
+
+    vec4 operator/(vec4 left, const vec4& right) {
+        return left.Divide(right);
+    }
+
+    vec4& vec4::operator+=(const vec4& other) {
+        return Add(other);
+    }
+
+    vec4& vec4::operator-=(const vec4& other) {
+        return Subtract(other);
+    }
+
+    vec4& vec4::operator*=(const vec4& other) {
+        return Multiply(other);
+    }
+
+    vec4& vec4::operator/=(const vec4& other) {
+        return Divide(other);
+    }
+
+    bool vec4::operator==(const vec4& other) {
+        return x == other.x && y == other.y && z == other.z && w == other.w;
+    }
+
+    bool vec4::operator!=(const vec4& other) {
+        return !(*this == other);
+    }
+
+    std::ostream& operator<<(std::ostream& stream, const vec4& vector)
+    {
+        stream << "vec4: {" << vector.x << ", " << vector.y << ", " << vector.z << ", " << vector.w << "}";
+        return stream;
+    }
+}

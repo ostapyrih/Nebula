e87edccbc2a4adebe188efeb0d7c378e88b9993a
Ostap Pyrih <ostapyrih@gmail.com>
2020-06-21 16:40:18 +0200

    extended vector2d functionality

--- src/math/vec2.cpp
+++ src/math/vec2.cpp
@@ -12,35 +12,59 @@ namespace nebula::math {
 		this->y = y;
 	}
 
-	vec2& vec2::add(const vec2& other) {
+	vec2& vec2::Add(const vec2& other) {
 		x += other.x;
 		y += other.y;
 
 		return *this;
 	}
 
-	vec2& vec2::subtract(const vec2& other) {
+	vec2& vec2::Subtract(const vec2& other) {
 		x -= other.x;
 		y -= other.y;
 
 		return *this;
 	}
 
-	vec2& vec2::multiply(const vec2& other) {
+	vec2& vec2::Multiply(const vec2& other) {
 		x *= other.x;
 		y *= other.y;
 
 		return *this;
 	}
 
-	vec2& vec2::divide(const vec2& other) {
+	vec2& vec2::Divide(const vec2& other) {
 		x /= other.x;
 		y /= other.y;
 
 		return *this;
 	}
 
-	std::ostream& operator<<(std::ostream& stream, const vec2& vector) {
+    vec2 operator+(vec2 left, const vec2& right) {
+        return left.Add(right);
+    }
+
+    vec2 operator-(vec2 left, const vec2& right) {
+        return left.Subtract(right);
+    }
+
+    vec2 operator*(vec2 left, const vec2& right) {
+        return left.Multiply(right);
+    }
+
+    vec2 operator/(vec2 left, const vec2& right) {
+        return left.Divide(right);
+    }
+
+    bool vec2::operator==(const vec2& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    bool vec2::operator!=(const vec2& other) const {
+        return !(*this == other);
+    }
+
+    std::ostream& operator<<(std::ostream& stream, const vec2& vector) {
 		return stream << "vec2: {" << vector.x << ", " << vector.y << "}";
 	}
 
--- src/math/vec2.h
+++ src/math/vec2.h
@@ -11,12 +11,18 @@ namespace nebula::math{
 		vec2();
 		vec2(const float& x, const float& y);
 
-		vec2& add(const vec2& other);
-		vec2& subtract(const vec2& other);
-		vec2& multiply(const vec2& other);
-		vec2& divide(const vec2& other);
+		vec2& Add(const vec2& other);
+		vec2& Subtract(const vec2& other);
+		vec2& Multiply(const vec2& other);
+		vec2& Divide(const vec2& other);
 
-        //todo overload operators +,-,*,/,==,!=
+        friend vec2 operator+(vec2 left, const vec2& right);
+        friend vec2 operator-(vec2 left, const vec2& right);
+        friend vec2 operator*(vec2 left, const vec2& right);
+        friend vec2 operator/(vec2 left, const vec2& right);
+
+        bool operator==(const vec2& other) const;
+        bool operator!=(const vec2& other) const;
 
         friend std::ostream& operator<<(std::ostream& stream, const vec2& vector);
 	};

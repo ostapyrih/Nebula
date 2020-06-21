c6cc63a7060c4dcd698503eeb9d5fb65d6a90924
Ostap Pyrih <ostapyrih@gmail.com>
2020-06-21 16:49:09 +0200

    Added vector4d

--- src/math/vec4.h
+++ src/math/vec4.h
@@ -2,6 +2,45 @@
 // Created by ostap on 6/21/2020.
 //
 
-struct vec4 {
+#include <iostream>
 
-};
+namespace nebula::math {
+
+    struct vec4 {
+        float x, y, z, w;
+
+        vec4();
+
+        vec4(float x, float y, float z, float w);
+
+        vec4 &Add(const vec4 &other);
+
+        vec4 &Subtract(const vec4 &other);
+
+        vec4 &Multiply(const vec4 &other);
+
+        vec4 &Divide(const vec4 &other);
+
+        friend vec4 operator+(vec4 left, const vec4 &right);
+
+        friend vec4 operator-(vec4 left, const vec4 &right);
+
+        friend vec4 operator*(vec4 left, const vec4 &right);
+
+        friend vec4 operator/(vec4 left, const vec4 &right);
+
+        bool operator==(const vec4 &other);
+
+        bool operator!=(const vec4 &other);
+
+        vec4 &operator+=(const vec4 &other);
+
+        vec4 &operator-=(const vec4 &other);
+
+        vec4 &operator*=(const vec4 &other);
+
+        vec4 &operator/=(const vec4 &other);
+
+        friend std::ostream &operator<<(std::ostream &stream, const vec4 &vector);
+    };
+}

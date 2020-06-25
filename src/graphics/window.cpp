15a6c0dfd1666d3656fd0acc0ba398d6a812ee88
Ostap Pyrih <ostapyrih@gmail.com>
2020-06-25 17:06:01 +0200

    added opengl error logging

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -71,6 +71,10 @@ namespace nebula::graphics
 
     void Window::update()
     {
+        GLenum error = glGetError();
+        if (error != GL_NO_ERROR)
+            std::cout << "OpenGL Error: " << error << std::endl;
+
         glfwPollEvents();
         glfwSwapBuffers(m_Window);
     }

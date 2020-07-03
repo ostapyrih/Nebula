11b16d5fc9c0bf2557aa2eb8eaf054a7b7432093
Ostap Pyrih <ostapyrih@gmail.com>
2020-07-03 02:13:17 +0200

    Added batch renderer

---
+++ src/graphics/BatchRenderer2D.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ostap on 6/26/2020.
+//
+
+#include "BatchRenderer2D.h"
---
+++ src/graphics/BatchRenderer2D.h
@@ -0,0 +1,15 @@
+//
+// Created by ostap on 6/26/2020.
+//
+
+#ifndef NEBULA_BATCHRENDERER2D_H
+#define NEBULA_BATCHRENDERER2D_H
+
+
+class BatchRenderer2D
+{
+
+};
+
+
+#endif //NEBULA_BATCHRENDERER2D_H
